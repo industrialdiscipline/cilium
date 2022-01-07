9a13290d818d6cab75463b61539753adab16502f
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@industrialdiscipline.com>
2022-01-07 21:52:26 +0100

    implement support of ICMP6 Error packet too big
    Running in masquerading BPF mode.
    
    Assuming that, Pod is sending a packet to the world of a size bigger
    than the accepted MTU in the networking path.
    
    A networking equipment in that networking path would return an ICMP
    Error packet with code PACKET_TOO_BIG. According to the RFE 5508,
    that networking equipment should embed the original packet in its
    response.
    
    Previously because of the missing support of PACKET_TOO_BIG, the NAT
    session to return the packet to Pod was never resolved. Meaning that
    the ICMP Error packet got dropped.
    
     a) The src IP of the packet becomes the networking equipment that is
     generating the error response.  b) The protocol used for the response
     is ICMP where the packet sent may be TDP/UPD.
    
    This change is updating the NAT process to read the embedded packet in
    the ICMP Error packet that to determine the NAT session and send back
    the packet to Pod.
    
    This change is for IPv6.
    
    Tests have been added in a way that the BPF_PROG will act as a
    networking equipment sending the ICMP Error packet.
    
    Fixes: #12968
    
    Signed-off-by: Sahid Orentino Ferdjaoui <sahid.ferdjaoui@industrialdiscipline.com>

--- bpf/tests/bpf_nat_tests.c
+++ bpf/tests/bpf_nat_tests.c
@@ -10,6 +10,7 @@
 #define DEBUG
 
 #include <lib/dbg.h>
+#include <lib/common.h>
 #include <lib/nat.h>
 
 __section("action/test-nat4-icmp-frag-needed-icmp")
@@ -125,6 +126,152 @@ int test_nat4_icmp_frag_needed_udp(struct __ctx_buff __maybe_unused *ctx)
 		goto out;
 
 	ret = snat_v4_process(ctx, NAT_DIR_INGRESS, &target, false);
+	cilium_dbg(ctx, DBG_UNSPEC, 1000, ret);
+out:
+	/* mark the termination of our program so that the go program stops
+	 * blocking on the ring buffer
+	 */
+	cilium_dbg(ctx, DBG_UNSPEC, 0xe3d, 0xe3d);
+	return ret;
+}
+
+__section("action/test-nat6-icmp-frag-needed-icmp")
+int test_nat6_icmp_frag_needed_icmp(struct __ctx_buff __maybe_unused *ctx)
+{
+	struct ipv6_nat_entry ostate;
+	struct ipv6_ct_tuple otuple = (struct ipv6_ct_tuple){
+		.saddr.p1 = bpf_htonl(0x20010db8),
+		.saddr.p2 = bpf_htonl(0x1),
+		.saddr.p3 = bpf_htonl(0x0),
+		.saddr.p4 = bpf_htonl(0x1),
+		.daddr.p1 = bpf_htonl(0x20010db8),
+		.daddr.p2 = bpf_htonl(0x2),
+		.daddr.p3 = bpf_htonl(0x0),
+		.daddr.p4 = bpf_htonl(0x1),
+		.sport = bpf_htons(123),
+		.nexthdr = IPPROTO_ICMPV6,
+	};
+	struct ipv6_nat_target target = {
+		.addr.p1 = bpf_htonl(0x20010db8),
+		.addr.p2 = bpf_htonl(0x3),
+		.addr.p3 = bpf_htonl(0x0),
+		.addr.p4 = bpf_htonl(0x1),
+		.min_port = NODEPORT_PORT_MIN_NAT,
+		.max_port = NODEPORT_PORT_MIN_NAT + 1,
+	};
+	int ret;
+
+	/* Assuming that, a Pod with IP(2001:db8:0:1::1) is sending a packet to
+	 * IP(2001:db8:0:2::1). To join the world the packet will pass through
+	 * Host(2001:db8:0:3::1).
+	 *
+	 * We instruct the SNAT table in such a way that the ICMP Error
+	 * packet generated in input should join Pod.
+	 */
+
+	ret = snat_v6_new_mapping(ctx, &otuple, &ostate, &target);
+	if (ret != 0)
+		goto out;
+
+	ret = snat_v6_process(ctx, NAT_DIR_INGRESS, &target);
+	cilium_dbg(ctx, DBG_UNSPEC, 1000, ret);
+out:
+	/* mark the termination of our program so that the go program stops
+	 * blocking on the ring buffer
+	 */
+	cilium_dbg(ctx, DBG_UNSPEC, 0xe3d, 0xe3d);
+	return ret;
+}
+
+__section("action/test-nat6-icmp-frag-needed-tcp")
+int test_nat6_icmp_frag_needed_tcp(struct __ctx_buff __maybe_unused *ctx)
+{
+	struct ipv6_nat_entry ostate;
+	struct ipv6_ct_tuple otuple = (struct ipv6_ct_tuple){
+		.saddr.p1 = bpf_htonl(0x20010db8),
+		.saddr.p2 = bpf_htonl(0x1),
+		.saddr.p3 = bpf_htonl(0x0),
+		.saddr.p4 = bpf_htonl(0x1),
+		.daddr.p1 = bpf_htonl(0x20010db8),
+		.daddr.p2 = bpf_htonl(0x2),
+		.daddr.p3 = bpf_htonl(0x0),
+		.daddr.p4 = bpf_htonl(0x1),
+		.sport = bpf_htons(3030),
+		.dport = bpf_htons(8080),
+		.nexthdr = IPPROTO_TCP,
+	};
+	struct ipv6_nat_target target = {
+		.addr.p1 = bpf_htonl(0x20010db8),
+		.addr.p2 = bpf_htonl(0x3),
+		.addr.p3 = bpf_htonl(0x0),
+		.addr.p4 = bpf_htonl(0x1),
+		.min_port = NODEPORT_PORT_MIN_NAT,
+		.max_port = NODEPORT_PORT_MIN_NAT + 1,
+	};
+	int ret;
+
+	/* Assuming that, a Pod with IP(2001:db8:0:1::1) is sending a packet to
+	 * IP(2001:db8:0:2::1). To join the world the packet will pass through
+	 * Host(2001:db8:0:3::1).
+	 *
+	 * We instruct the SNAT table in such a way that the ICMP Error
+	 * packet generated in input should join Pod.
+	 */
+
+	ret = snat_v6_new_mapping(ctx, &otuple, &ostate, &target);
+	if (ret != 0)
+		goto out;
+
+	ret = snat_v6_process(ctx, NAT_DIR_INGRESS, &target);
+	cilium_dbg(ctx, DBG_UNSPEC, 1000, ret);
+out:
+	/* mark the termination of our program so that the go program stops
+	 * blocking on the ring buffer
+	 */
+	cilium_dbg(ctx, DBG_UNSPEC, 0xe3d, 0xe3d);
+	return ret;
+}
+
+__section("action/test-nat6-icmp-frag-needed-udp")
+int test_nat6_icmp_frag_needed_udp(struct __ctx_buff __maybe_unused *ctx)
+{
+	struct ipv6_nat_entry ostate;
+	struct ipv6_ct_tuple otuple = (struct ipv6_ct_tuple){
+		.saddr.p1 = bpf_htonl(0x20010db8),
+		.saddr.p2 = bpf_htonl(0x1),
+		.saddr.p3 = bpf_htonl(0x0),
+		.saddr.p4 = bpf_htonl(0x1),
+		.daddr.p1 = bpf_htonl(0x20010db8),
+		.daddr.p2 = bpf_htonl(0x2),
+		.daddr.p3 = bpf_htonl(0x0),
+		.daddr.p4 = bpf_htonl(0x1),
+		.sport = bpf_htons(3030),
+		.dport = bpf_htons(8080),
+		.nexthdr = IPPROTO_UDP,
+	};
+	struct ipv6_nat_target target = {
+		.addr.p1 = bpf_htonl(0x20010db8),
+		.addr.p2 = bpf_htonl(0x3),
+		.addr.p3 = bpf_htonl(0x0),
+		.addr.p4 = bpf_htonl(0x1),
+		.min_port = NODEPORT_PORT_MIN_NAT,
+		.max_port = NODEPORT_PORT_MIN_NAT + 1,
+	};
+	int ret;
+
+	/* Assuming that, a Pod with IP(2001:db8:0:1::1) is sending a packet to
+	 * IP(2001:db8:0:2::1). To join the world the packet will pass through
+	 * Host(2001:db8:0:3::1).
+	 *
+	 * We instruct the SNAT table in such a way that the ICMP Error
+	 * packet generated in input should join Pod.
+	 */
+
+	ret = snat_v6_new_mapping(ctx, &otuple, &ostate, &target);
+	if (ret != 0)
+		goto out;
+
+	ret = snat_v6_process(ctx, NAT_DIR_INGRESS, &target);
 	cilium_dbg(ctx, DBG_UNSPEC, 1000, ret);
 out:
 	/* mark the termination of our program so that the go program stops
