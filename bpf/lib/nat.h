4fe19846994daf190e39d1ff6a8eb80be98a67f2
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@industrialdiscipline.com>
2022-01-07 10:53:39 +0100

    refactor icmp4 handler to support future features
    This is a convenient patch which will be used when implementing ICMP
    NEED TO FRAG.
    
    Signed-off-by: Sahid Orentino Ferdjaoui <sahid.ferdjaoui@industrialdiscipline.com>

--- bpf/lib/nat.h
+++ bpf/lib/nat.h
@@ -542,16 +542,19 @@ snat_v4_process(struct __ctx_buff *ctx, enum nat_dir dir,
 	case IPPROTO_ICMP:
 		if (ctx_load_bytes(ctx, off, &icmphdr, sizeof(icmphdr)) < 0)
 			return DROP_INVALID;
-		if (icmphdr.type != ICMP_ECHO &&
-		    icmphdr.type != ICMP_ECHOREPLY)
-			return DROP_NAT_UNSUPP_PROTO;
-		if (icmphdr.type == ICMP_ECHO) {
+
+		switch (icmphdr.type) {
+		case ICMP_ECHO:
 			tuple.dport = 0;
 			tuple.sport = icmphdr.un.echo.id;
-		} else {
+			break;
+		case ICMP_ECHOREPLY:
 			tuple.dport = icmphdr.un.echo.id;
 			tuple.sport = 0;
 			icmp_echoreply = true;
+			break;
+		default:
+			return DROP_NAT_UNSUPP_PROTO;
 		}
 		break;
 	default:
